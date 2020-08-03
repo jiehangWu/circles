e9b8c41efc51f01f2197b486491f81858e6f7789
jiehangWu <jiehangwu.cs@gmail.com>
2020-08-02 21:20:06 -0700

    change wss url

--- frontend/src/components/chat/socket.js
+++ frontend/src/components/chat/socket.js
@@ -1,6 +1,6 @@
 import SockJS from 'sockjs';
 
-const url = 'wss://https://circles-ubc-api.azurewebsites.net:5000';
+const url = 'wss://https://circles-ubc-api.azurewebsites.net';
 let socket;
 const startSocket = () => {
     if (!window.WebSocket) {
