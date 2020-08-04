e656fd82bf1c995fd8aab3bb7155ef186ac9cba7
wangyf97 <cufewangyifan@qq.com>
2020-08-03 22:07:21 -0700

    try another rehydrate level

--- frontend/src/helpers/store.js
+++ frontend/src/helpers/store.js
@@ -3,10 +3,12 @@ import {persistStore, persistReducer} from "redux-persist";
 import storage from 'redux-persist/lib/storage'
 import thunkMiddleware from 'redux-thunk';
 import {rootReducer} from '../reducers';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
 
 const persistConfig = {
     key: 'root',
     storage,
+    stateReconciler: autoMergeLevel2,
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
