b42992053c1cd9354e262023c32f6050ddf35700
wangyf97 <cufewangyifan@qq.com>
2020-08-03 21:29:57 -0700

    persist reducer

--- frontend/src/helpers/store.js
+++ frontend/src/helpers/store.js
@@ -1,10 +1,22 @@
-import { createStore, applyMiddleware } from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import {persistStore, persistReducer} from "redux-persist";
+import storage from 'redux-persist/lib/storage'
 import thunkMiddleware from 'redux-thunk';
-import { rootReducer } from '../reducers';
+import {rootReducer} from '../reducers';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 
 export const store = createStore(
-  rootReducer,
-  applyMiddleware(
-    thunkMiddleware,
-  ),
+    persistedReducer,
+    applyMiddleware(
+        thunkMiddleware,
+    ),
 );
+
+export const persistor = persistStore(store);
